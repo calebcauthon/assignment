72e595b1defcd93844b47083ed1cd3316f5de1da
caleb <calebcauthon@gmail.com>
2022-01-13 20:23:04 -0600

    Get the category name from the input

--- AveragePriceCalculator.groovy
+++ AveragePriceCalculator.groovy
@@ -1,13 +1,16 @@
 class AveragePriceCalculator
 {
-  public static Map getAveragePrices(products, b, margins)
+  public static Map getAveragePrices(products, categories, margins)
   {
     def product = products[0]
     def name = product[0]
     def group = product[1]
     def cost = product[2]
 
-    def margin = margins["the-category"].replace("%", "");
+    def category = categories[0];
+    def categoryName = category[0];
+
+    def margin = margins[categoryName].replace("%", "");
     def markup = Float.parseFloat(margin);
     def price = cost * (1 + markup / 100);
 
